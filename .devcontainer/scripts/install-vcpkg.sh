3ab0c590804110d75f9f7487c0cc54ef32d708b2
Roy Kid <lijichen365@gmail.com>
2024-07-24 10:20:06 +0000

    change base image

--- .devcontainer/scripts/install-vcpkg.sh
+++ .devcontainer/scripts/install-vcpkg.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+#-------------------------------------------------------------------------------------------------------------
+
 set -e
 
 USERNAME=${1:-"vscode"}
