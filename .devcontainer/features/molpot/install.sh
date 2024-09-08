5ba79387bdf46af5fe7a1b730a56dbe122510116
Roy Kid <lijichen365@gmail.com>
2024-09-08 14:33:09 +0000

    add cuda flag for molpot

--- .devcontainer/features/molpot/install.sh
+++ .devcontainer/features/molpot/install.sh
@@ -1,1 +1,18 @@
-#!/usr/bin/zsh
+#!/bin/sh
+set -e
+
+echo "Activating feature 'pytorch'"
+
+which pip > /dev/null || (apt update && apt install python3-pip -y -qq)
+
+if [ "${enable_cuda}" == "true" ]; then
+    echo "Installing pytorch with CUDA support"
+    pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu${cudaVersion}
+    echo "Pytorch with CUDA support installed!"
+else
+    echo "Installing pytorch without CUDA support"
+    pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+    echo "Pytorch without CUDA support installed!"
+fi
+
+echo "pytorch installed! "
